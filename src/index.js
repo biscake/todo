ace87220bff27261a2790000c53825851a69d373
biscake <leonut.ljh@gmail.com>
2024-05-09 00:08:11 +0800

    Added function to add edit button to all to-do list

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import toDo, { projectsController } from "./listModule.js";
 import ScreenController from "./screenController.js";
 
 toDo.createToDo("a", "a", " b", " c");
-
+toDo.createToDo("a", "a", " b", " c");
 projectsController.createNewProject("new");
 
 window.onload = () => {
